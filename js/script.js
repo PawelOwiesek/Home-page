3e0381c613fda737ef1800a6c79c71b2d9da05f0
Pawel Owiesek <owiesek.p@gmail.com>
2023-01-05 17:04:01 +0100

    added welcome message and three functions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-let buttonElement = document.querySelector(".js-button");
-let header__themName = document.querySelector(".js-header__themeName");
-let bodyElement = document.querySelector(".body");
+{
+    const welcomeMessage = () => {
+        console.log("Hello");
+    }
 
-buttonElement.addEventListener("click", () => {
-    bodyElement.classList.toggle("light");
-    header__themName.innerText = bodyElement.classList.contains("light") ? "dark" : "light";
-});
+    const changeBackground = () => {
+        const header__themName = document.querySelector(".js-header__themeName");
+        const bodyElement = document.querySelector(".body");
+        bodyElement.classList.toggle("light");
+        header__themName.innerText = bodyElement.classList.contains("light") ? "dark" : "light";
+    }
+
+    const init = () => {
+        const buttonElement = document.querySelector(".js-button");
+        buttonElement.addEventListener("click", changeBackground);
+        welcomeMessage();
+    }
+    init();
+}

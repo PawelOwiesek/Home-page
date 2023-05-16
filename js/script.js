516da0edc4a5e39c4ed4265f23a9f849a186a069
Pawel Owiesek <Owiesek.p@gmail.com>
2023-05-16 11:00:36 +0200

    code formating

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 {
-    const welcomeMessage = () => {
-        console.log("Hello");
-    }
+  const welcomeMessage = () => {
+    console.log("Hello");
+  };
 
-    const changeBackground = () => {
-        const headerThemeName = document.querySelector(".js-headerThemeName");
-        const bodyElement = document.querySelector(".body");
-        bodyElement.classList.toggle("light");
-        headerThemeName.innerText = bodyElement.classList.contains("light") ? "dark" : "light";
-    }
+  const changeBackground = () => {
+    const headerThemeName = document.querySelector(".js-headerThemeName");
+    const bodyElement = document.querySelector(".body");
+    bodyElement.classList.toggle("light");
+    headerThemeName.innerText = bodyElement.classList.contains("light")
+      ? "dark"
+      : "light";
+  };
 
-    const init = () => {
-        const buttonElement = document.querySelector(".js-button");
-        buttonElement.addEventListener("click", changeBackground);
-        welcomeMessage();
-    }
-    init();
+  const init = () => {
+    const buttonElement = document.querySelector(".js-button");
+    buttonElement.addEventListener("click", changeBackground);
+    welcomeMessage();
+  };
+  init();
 }
